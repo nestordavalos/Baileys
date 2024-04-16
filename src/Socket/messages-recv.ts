4de1a6e2518be5aaf928288657daaef038d49a75
nestordavalos <nestordavalos@gmail.com>
2024-04-16 00:39:13 -0400

    fix(missing messages): revert to normal

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -683,12 +683,12 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 	}
 
 	const handleMessage = async(node: BinaryNode) => {
-		if(getBinaryNodeChild(node, 'unavailable') && !getBinaryNodeChild(node, 'enc')) {
-			// "missing message from node" fix
-			logger.debug(node, 'missing body; sending ack then ignoring.')
-			await sendMessageAck(node)
-			return
-		}
+		// if(getBinaryNodeChild(node, 'unavailable') && !getBinaryNodeChild(node, 'enc')) {
+		// 	// "missing message from node" fix
+		// 	logger.debug(node, 'missing body; sending ack then ignoring.')
+		// 	await sendMessageAck(node)
+		// 	return
+		// }
 
 		const { fullMessage: msg, category, author, decrypt } = decryptMessageNode(
 			node,
