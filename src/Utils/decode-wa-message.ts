7d324020378603fe30983e0d1c572b3eecdf0d14
nestordavalos <nestordavalos@gmail.com>
2024-05-06 14:02:37 -0400

    update

--- src/Utils/decode-wa-message.ts
+++ src/Utils/decode-wa-message.ts
@@ -5,7 +5,6 @@ import { SignalRepository, WAMessageKey } from '../Types'
 import { areJidsSameUser, BinaryNode, isJidBroadcast, isJidGroup, isJidStatusBroadcast, isJidUser, isLidUser } from '../WABinary'
 import { BufferJSON, unpadRandomMax16 } from './generics'
 
-
 const NO_MESSAGE_FOUND_ERROR_TEXT = 'Message absent from node'
 
 type MessageType = 'chat' | 'peer_broadcast' | 'other_broadcast' | 'group' | 'direct_peer_status' | 'other_status'
