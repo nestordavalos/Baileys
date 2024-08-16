b465593f38a1f3082b7fd46c9982785a7e815316
nestordavalos <nestordavalos@gmail.com>
2024-08-15 21:18:03 -0400

    fix

--- src/Types/Message.ts
+++ src/Types/Message.ts
@@ -125,6 +125,14 @@ export type ButtonReplyInfo = {
     index: number
 }
 
+export type GroupInviteInfo = {
+    inviteCode: string
+    inviteExpiration: number
+    text: string
+    jid: string
+    subject: string
+}
+
 export type WASendableProduct = Omit<proto.Message.ProductMessage.IProductSnapshot, 'productImage'> & {
     productImage: WAMediaUpload
 }
@@ -154,7 +162,18 @@ export type AnyRegularMessageContent = (
         type: 'template' | 'plain'
     }
     | {
+        groupInvite: GroupInviteInfo
+    }
+    | {
         listReply: Omit<proto.Message.IListResponseMessage, 'contextInfo'>
+    }
+    | {
+        pin: WAMessageKey
+        type: proto.PinInChat.Type
+        /**
+         * 24 hours, 7 days, 30 days
+         */
+        time?: 86400 | 604800 | 2592000
     }
     | {
         product: WASendableProduct
@@ -236,6 +255,7 @@ export type MediaGenerationOptions = {
 }
 export type MessageContentGenerationOptions = MediaGenerationOptions & {
 	getUrlInfo?: (text: string) => Promise<WAUrlInfo | undefined>
+    getProfilePicUrl?: (jid: string, type: 'image' | 'preview') => Promise<string | undefined>
 }
 export type MessageGenerationOptions = MessageContentGenerationOptions & MessageGenerationOptionsFromContent
 
