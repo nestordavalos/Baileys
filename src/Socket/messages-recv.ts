0465cf004ecca48fcb700a4e77e7627fcad43305
nestordavalos <mestordavalos@gmail.com>
2024-10-07 17:42:47 -0300

    feat: add offerCall and terminateCall functions

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -16,6 +16,7 @@ import {
 	derivePairingCodeKey,
 	encodeBigEndian,
 	encodeSignedDeviceIdentity,
+	generateMessageIDV2,
 	getCallStatusFromNode,
 	getHistoryMsg,
 	getNextPreKeys,
@@ -37,6 +38,7 @@ import {
 	isJidGroup, isJidStatusBroadcast,
 	isJidUser,
 	jidDecode,
+	jidEncode,
 	jidNormalizedUser,
 	S_WHATSAPP_NET
 } from '../WABinary'
@@ -68,6 +70,8 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		sendReceipt,
 		uploadPreKeys,
 		sendPeerDataOperationMessage,
+		createParticipantNodes,
+		getUSyncDevices,
 	} = sock
 
 	/** this mutex ensures that each retryRequest will wait for the previous one to finish */
@@ -119,12 +123,95 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		await sendNode(stanza)
 	}
 
+	const offerCall = async(toJid: string, isVideo = false) => {
+		const callId = randomBytes(16).toString('hex').toUpperCase().substring(0, 64)
+
+		const offerContent: BinaryNode[] = []
+		offerContent.push({ tag: 'audio', attrs: { enc: 'opus', rate: '16000' }, content: undefined })
+		offerContent.push({ tag: 'audio', attrs: { enc: 'opus', rate: '8000' }, content: undefined })
+
+		if(isVideo) {
+			offerContent.push({
+				tag: 'video',
+				attrs: { enc: 'vp8', dec: 'vp8', orientation: '0', 'screen_width': '1920', 'screen_height': '1080', 'device_orientation': '0' },
+				content: undefined
+			})
+		}
+
+		offerContent.push({ tag: 'net', attrs: { medium: '3' }, content: undefined })
+		offerContent.push({ tag: 'capability', attrs: { ver: '1' }, content: new Uint8Array([1, 4, 255, 131, 207, 4]) })
+		offerContent.push({ tag: 'encopt', attrs: { keygen: '2' }, content: undefined })
+
+		const encKey = randomBytes(32)
+
+		const devices = (await getUSyncDevices([toJid], true, false)).map(({ user, device }) => jidEncode(user, 's.whatsapp.net', device))
+
+		await assertSessions(devices, true)
+
+		const { nodes: destinations, shouldIncludeDeviceIdentity } = await createParticipantNodes(devices, {
+			call: {
+				callKey: new Uint8Array(encKey)
+			}
+		}, { count: '0' })
+
+		offerContent.push({ tag: 'destination', attrs: {}, content: destinations })
+
+		if(shouldIncludeDeviceIdentity) {
+			offerContent.push({
+				tag: 'device-identity',
+				attrs: {},
+				content: encodeSignedDeviceIdentity(authState.creds.account!, true)
+			})
+		}
+
+		const stanza: BinaryNode = ({
+			tag: 'call',
+			attrs: {
+				id: generateMessageIDV2(),
+				to: toJid,
+			},
+			content: [{
+				tag: 'offer',
+				attrs: {
+					'call-id': callId,
+					'call-creator': authState.creds.me!.id,
+				},
+				content: offerContent,
+			}],
+		})
+		await query(stanza)
+		return {
+			id: callId,
+			to: toJid
+		}
+	}
+
+	const terminateCall = async(callId: string, toJid: string) => {
+		const stanza: BinaryNode = ({
+			tag: 'call',
+			attrs: {
+				id: generateMessageIDV2(),
+				to: toJid,
+			},
+			content: [{
+			    tag: 'terminate',
+			    attrs: {
+					'call-id': callId,
+					'call-creator': toJid,
+			    },
+			    content: undefined,
+			}],
+		})
+		await query(stanza)
+	}
+
 	const rejectCall = async(callId: string, callFrom: string) => {
 		const stanza: BinaryNode = ({
 			tag: 'call',
 			attrs: {
-				from: authState.creds.me!.id,
+				id: generateMessageIDV2(),
 				to: callFrom,
+				from: authState.creds.me!.id,
 			},
 			content: [{
 			    tag: 'reject',
@@ -342,7 +429,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 
 			break
 		case 'membership_approval_mode':
-			const approvalMode: any = getBinaryNodeChild(child, 'group_join')
+			const approvalMode = getBinaryNodeChild(child, 'group_join')
 			if(approvalMode) {
 				msg.messageStubType = WAMessageStubType.GROUP_MEMBERSHIP_JOIN_APPROVAL_MODE
 				msg.messageStubParameters = [ approvalMode.attrs.state ]
