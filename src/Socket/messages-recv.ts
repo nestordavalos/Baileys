45036d7de8cc686af3258f87e934d3314237a97c
nestordavalos <mestordavalos@gmail.com>
2025-01-30 18:18:36 -0300

    Agregar lógica de reintentos para el reenvío de mensajes en caso de fallos

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -1017,10 +1017,19 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		// it means -- the message hasn't reached all devices yet
 		// we'll retry sending the message here
 		if(attrs.phash) {
+			const maxRetries = 3; // Número máximo de reintentos
+			const retryCount = msgRetryCache.get<number>(key.id!) || 0;
+		
+			if(retryCount >= maxRetries) {
+				logger.warn({ attrs }, 'max retries reached, not resending message');
+				return;
+			}
+		
 			logger.info({ attrs }, 'received phash in ack, resending message...')
 			const msg = await getMessage(key)
 			if(msg) {
 				await relayMessage(key.remoteJid!, msg, { messageId: key.id!, useUserDevicesCache: false })
+				msgRetryCache.set(key.id!, retryCount + 1); // Incrementar el contador de reintentos
 			} else {
 				logger.warn({ attrs }, 'could not send message again, as it was not found')
 			}
