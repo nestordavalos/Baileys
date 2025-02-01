5bc5b9ad4f1eb9d231a7f5e7a138bfe9ecdbeeac
nestordavalos <mestordavalos@gmail.com>
2025-02-01 10:24:06 -0300

    Refactor manejo de nodos en el socket: eliminar parámetros innecesarios y optimizar el procesamiento de mensajes

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -688,7 +688,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		}
 	}
 
-	const handleReceipt = async(node: BinaryNode, offline: boolean) => {
+	const handleReceipt = async(node: BinaryNode) => {
 		const { attrs, content } = node
 		const isLid = attrs.from.includes('lid')
 		const isNodeFromMe = areJidsSameUser(attrs.participant || attrs.from, isLid ? authState.creds.me?.lid : authState.creds.me?.id)
@@ -780,7 +780,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		}
 	}
 
-	const handleNotification = async(node: BinaryNode, offline: boolean) => {
+	const handleNotification = async(node: BinaryNode) => {
 		const remoteJid = node.attrs.from
 		if(shouldIgnoreJid(remoteJid) && remoteJid !== '@s.whatsapp.net') {
 			logger.debug({ remoteJid, id: node.attrs.id }, 'ignored notification')
@@ -816,12 +816,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 		}
 	}
 
-	const handleMessage = async(node: BinaryNode, offline: boolean) => {
-  	if(offline) {
-    	await sendMessageAck(node)
-			return
-  	}
-
+	const handleMessage = async(node: BinaryNode) => {
 		if(shouldIgnoreJid(node.attrs.from) && node.attrs.from !== '@s.whatsapp.net') {
 			logger.debug({ key: node.attrs.key }, 'ignored message')
 			await sendMessageAck(node)
@@ -1030,9 +1025,11 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 
 	const handleBadAck = async({ attrs }: BinaryNode) => {
 		const key: WAMessageKey = { remoteJid: attrs.from, fromMe: true, id: attrs.id }
-		// current hypothesis is that if pash is sent in the ack
-		// it means -- the message hasn't reached all devices yet
-		// we'll retry sending the message here
+
+		// WARNING: REFRAIN FROM ENABLING THIS FOR NOW. IT WILL CAUSE A LOOP
+		// // current hypothesis is that if pash is sent in the ack
+		// // it means -- the message hasn't reached all devices yet
+		// // we'll retry sending the message here
 		// if(attrs.phash) {
 		// 	logger.info({ attrs }, 'received phash in ack, resending message...')
 		// 	const msg = await getMessage(key)
@@ -1089,7 +1086,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 	}
 
 	const makeOfflineNodeProcessor = () => {
-		const nodeProcessorMap: Map<MessageType, (node: BinaryNode, offline: boolean) => Promise<void>> = new Map([
+		const nodeProcessorMap: Map<MessageType, (node: BinaryNode) => Promise<void>> = new Map([
 			['message', handleMessage],
 			['call', handleCall],
 			['receipt', handleReceipt],
@@ -1121,7 +1118,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 						continue
 					}
 
-					await nodeProcessor(node, true)
+					await nodeProcessor(node)
 				}
 
 				isProcessing = false
@@ -1135,7 +1132,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 
 	const offlineNodeProcessor = makeOfflineNodeProcessor()
 
-	const processNode = (type: MessageType, node: BinaryNode, identifier: string, exec: (node: BinaryNode, offline: boolean) => Promise<void>) => {
+	const processNode = (type: MessageType, node: BinaryNode, identifier: string, exec: (node: BinaryNode) => Promise<void>) => {
 		const isOffline = !!node.attrs.offline
 
 		if(isOffline) {
