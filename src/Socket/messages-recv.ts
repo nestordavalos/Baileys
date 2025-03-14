4c3c60854b886c250e974064b3546bd41717720a
nestordavalos <mestordavalos@gmail.com>
2025-03-14 01:28:42 -0300

    Eliminar archivos obsoletos, actualizar el año de copyright y deprecar la opción printQRInTerminal

--- src/Socket/messages-recv.ts
+++ src/Socket/messages-recv.ts
@@ -934,7 +934,7 @@ export const makeMessagesRecvSocket = (config: SocketConfig) => {
 			throw new Boom('Not authenticated')
 		}
 
-		const pdoMessage = {
+		const pdoMessage: proto.Message.IPeerDataOperationRequestMessage = {
 			historySyncOnDemandRequest: {
 				chatJid: oldestMsgKey.remoteJid,
 				oldestMsgFromMe: oldestMsgKey.fromMe,
