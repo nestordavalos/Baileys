7836783442327b0affa841fcf6b29958f017cb1f
nestordavalos <mestordavalos@gmail.com>
2025-03-17 10:47:56 -0300

    Actualizar la URL de la homepage en package.json, corregir el tipo de id en el socket de mensajes y actualizar dependencias en yarn.lock

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -523,7 +523,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 				const stanza: BinaryNode = {
 					tag: 'message',
 					attrs: {
-						id: msgId!,
+						id: msgId,
 						type: getMessageType(message),
 						...(additionalAttributes || {})
 					},
