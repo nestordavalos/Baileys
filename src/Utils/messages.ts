8d78a5b75704c5bde607ff519a18ccceca0d2fa0
nestordavalos <nestordavalos@gmail.com>
2025-06-03 22:32:59 -0300

    fix: validar la existencia de directPath en el mensaje de contenido multimedia antes de la descarga

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -877,7 +877,14 @@ export const downloadMediaMessage = async <Type extends 'buffer' | 'stream'>(
 			}
 			mediaType = 'thumbnail-link'
 		} else {
-			download = media
+			if (!('directPath' in media) || typeof media.directPath !== 'string') {
+				throw new Boom(`"${contentType}" message does not have a directPath`, { statusCode: 400, data: media });
+			}
+			download = {
+				mediaKey: media.mediaKey,
+				directPath: media.directPath,
+				url: 'https://mmg.whatsapp.net' + media.directPath
+			};
 		}
 
 		const stream = await downloadContentFromMessage(download, mediaType, options)
